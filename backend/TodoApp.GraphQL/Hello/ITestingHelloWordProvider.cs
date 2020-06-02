2e33f06c06a9a974dd7050ed9fb19c1791f1d8f4
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-06-02 13:03:59 +0200

    Cleanup

--- backend/TodoApp.GraphQL/Hello/ITestingHelloWordProvider.cs
+++ backend/TodoApp.GraphQL/Hello/ITestingHelloWordProvider.cs
@@ -1,8 +1,6 @@
-using System;
-
 namespace TodoApp.GraphQL.Hello
 {
-    public interface ITestingHelloWordProvider
+	public interface ITestingHelloWordProvider
     {
         string HelloWord { get; set; }
 	}
