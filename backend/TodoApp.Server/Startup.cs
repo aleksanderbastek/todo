04dff0f0da9a5405a8c9c1eaf41fd11d6bb9ae6d
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-06-08 17:31:41 +0200

    Enable CORS for all requests

--- backend/TodoApp.Server/Startup.cs
+++ backend/TodoApp.Server/Startup.cs
@@ -13,13 +13,24 @@ namespace TodoApp.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGraphQLSetup();
+			services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+			{
+				builder
+					.AllowAnyOrigin()
+					.AllowAnyMethod()
+					.AllowAnyHeader();
+
+			}));
+
+			services.AddGraphQLSetup();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+			app.UseCors("AllowAll");
+
+			if (env.IsDevelopment())
             {
                 app
                     .UseDeveloperExceptionPage()
