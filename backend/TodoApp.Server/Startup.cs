2a42f5a889d76d36d7acf9f63fc057b7305a8aff
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-06-14 17:30:12 +0200

    Add automatic ef migrations merging, register necessary services

--- backend/TodoApp.Server/Startup.cs
+++ backend/TodoApp.Server/Startup.cs
@@ -1,9 +1,20 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using TodoApp.Cqrs;
+using TodoApp.Domain.Contexts;
+using TodoApp.Domain.Repositories.Abstractions.Readable;
+using TodoApp.Domain.Repositories.Abstractions.Writable;
+using TodoApp.Domain.Repositories.Concretes.Readable;
+using TodoApp.Domain.Repositories.Concretes.Writable;
 using TodoApp.Graphql;
 
 namespace TodoApp.Server
@@ -14,6 +25,20 @@ namespace TodoApp.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddDbContext<TodoDbContext>(optionsBuilder =>
+				optionsBuilder.UseNpgsql(
+					"Host=pg-db;Database=todo_app;Username=admin;Password=SimplePassword1"
+				)
+			);
+
+			services
+				// Board repositories
+				.AddScoped<IWritableBoardRepository, WritableBoardRepository>()
+				.AddScoped<IReadableBoardRepository, ReadableBoardRepository>()
+				// Todo repositories
+				.AddScoped<IWritableTodoRepository, WritableTodoRepository>()
+				.AddScoped<IReadableTodoRepository, ReadableTodoRepository>();
+
 			services.AddCors(o => o.AddPolicy("AllowAll", builder =>
 			{
 				builder
@@ -30,6 +55,44 @@ namespace TodoApp.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+			using (var serviceScope = app.ApplicationServices.CreateScope()) {
+				var logger = serviceScope.ServiceProvider.GetService<ILogger<Startup>>();
+				var context = serviceScope.ServiceProvider.GetService<TodoDbContext>();
+
+				var pendingMigrationsCount = context.Database.GetPendingMigrations().Count();
+				var migrationsPluralForm = "migration" + (pendingMigrationsCount > 1 ? "s" : "");
+
+				if (pendingMigrationsCount > 0) {
+					logger.LogWarning(
+                        $"Merging {pendingMigrationsCount} pending " +
+                        $"{migrationsPluralForm} into database"
+                    );
+
+					context.Database.Migrate();
+
+					logger.LogWarning("Database migration complete");
+				}
+
+				var migrationsAssembly = context.GetService<IMigrationsAssembly>();
+				var differ = context.GetService<IMigrationsModelDiffer>();
+
+				var modelSnapshotIsDifferent = differ.HasDifferences(
+					migrationsAssembly.ModelSnapshot.Model,
+					context.Model
+				);
+
+				if (modelSnapshotIsDifferent) {
+					var errorMessage =
+						"Actual model snapshot differs ModelSnapshot applied to database!\n" +
+						"Model must be up to date with the latest ModelSnapshot to run this application.\n" +
+						"Review your changes and add missing migration using following command:\n" +
+						"dotnet ef migrations add MigrationName -p TodoApp.Domain.Contexts --startup-project TodoApp.Server";
+
+					logger.LogCritical(errorMessage);
+					throw new Exception(errorMessage);
+				}
+			}
+
 			app.UseCors("AllowAll");
 
 			if (env.IsDevelopment())
