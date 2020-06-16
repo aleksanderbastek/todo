5bd5e70079486606c00da4da4d9f5492db073de0
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-06-16 21:50:05 +0200

    Separate UpdateBoardInfoCommand into two specialized commands

---
+++ backend/TodoApp.Domain.Handlers/Commands/Board/UpdateBoardDescriptionResult.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TodoApp.Domain.Handlers.Commands.Board
+{
+    public class UpdateBoardDescriptionResult
+    {
+		public string BoardId { get; set; }
+	}
+}
--- backend/TodoApp.Domain.Handlers/Commands/Board/UpdateBoardTitleCommand.cs
+++ backend/TodoApp.Domain.Handlers/Commands/Board/UpdateBoardTitleCommand.cs
@@ -1,11 +1,11 @@
 using System;
+using TodoApp.Cqrs.Types.Abstract;
 
 namespace TodoApp.Domain.Handlers.Commands.Board
 {
-    public class UpdateBoardInfoResult
+    public class UpdateBoardTitleCommand: ICommand<UpdateBoardTitleResult>
     {
 		public string BoardId { get; set; }
 		public string Title { get; set; }
-		public string Description { get; set; }
 	}
 }
