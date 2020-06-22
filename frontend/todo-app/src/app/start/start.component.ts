f93078e576c3ee7925ecbf13256598aa9e518db0
aleksanderbastek <officialbastek@gmail.com>
2020-06-22 13:37:42 +0200

    test graphql

--- frontend/todo-app/src/app/start/start.component.ts
+++ frontend/todo-app/src/app/start/start.component.ts
@@ -1,30 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../api.service";
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+
+import { createBo } from "../graphql/mutations";
+import { variable } from "@angular/compiler/src/output/output_ast";
 
 @Component({
-  selector: 'app-start',
-  templateUrl: './start.component.html',
-  styleUrls: ['./start.component.css']
+  selector: "app-start",
+  templateUrl: "./start.component.html",
+  styleUrls: ["./start.component.css"]
 })
 export class StartComponent implements OnInit {
+
+  title: String = "fourth";
 
   constructor(private api: ApiService, private apollo: Apollo) { }
 
   ngOnInit(): void {
+
+    const dupa = gql`
+      query dupa {
+
+      }
+
+    `;
+
+    this.apollo;
+
+    this.apollo.mutate<any>({
+      mutation: createBo,
+      variables: {
+        title: this.title,
+      },
+    })
+      .subscribe(
+        (data: any) => console.log(data)
+      );
     // this.api.getHelloWorld();
+		/*
 
-    this.apollo
-      .watchQuery({
-        query: gql` {
-    sayHelloWorld
-
-}`,
+  }
+        }`,
       })
       .valueChanges
-      .subscribe((data) => {
-        console.log(data);
+      .subscribe((r: any) => {
+        this.descrpt = r.data.board.id;
+        console.log(r);
       });
+    setTimeout(() => { console.log(this.descrpt) }, 500);
+    */
+
   }
+
 }
