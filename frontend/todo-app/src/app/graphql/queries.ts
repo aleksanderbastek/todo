5e2f212877043482c8719f73bfd0d5e5446b5ea9
aleksanderbastek <officialbastek@gmail.com>
2020-06-23 13:53:47 +0200

    dodanie i testowanie queries

--- frontend/todo-app/src/app/graphql/queries.ts
+++ frontend/todo-app/src/app/graphql/queries.ts
@@ -1,11 +1,80 @@
 import gql from "graphql-tag";
+// import { query } from '@angular/animations';
 
 export const showBoard = gql`
-query showBoard($id: String){
+query showBoard($id: String!){
   board(id:$id){
     id
     title
     description
     creationDate
+    numberOfTodos
+    numberOfDoneTodos
+    numberOfUndoneTodos
   }
 }`;
+
+export const getTodos = gql`
+query getTodos($id:String!, $take: Int!,$skip:Int)
+{
+  board(id: $id) {
+    todos(take: $take, skip: $skip) {
+      id
+      title
+      deadline
+      creationDate
+      isDone
+      isExpired
+      doneDate
+    }
+  }
+}`;
+
+export const getDoneTodos = gql`
+query getDoneTodos($id:String!, $take: Int!,$skip:Int)
+{
+  board(id: $id) {
+    doneTodos(take: $take, skip: $skip) {
+      id
+      title
+      deadline
+      creationDate
+      isDone
+      isExpired
+      doneDate
+    }
+  }
+}`;
+
+export const getUndoneTodos = gql`
+query getUndoneTodos($id:String!, $take: Int!,$skip:Int)
+{
+  board(id: $id) {
+    undoneTodos(take: $take, skip: $skip) {
+      id
+      title
+      deadline
+      creationDate
+      isDone
+      isExpired
+      doneDate
+    }
+  }
+}`;
+
+export const getTodo = gql`
+query getTodo($id:String!)
+{
+todo(id: $id) {
+      id
+      title
+      deadline
+      creationDate
+      isDone
+      isExpired
+      doneDate
+
+  }
+}`;
+
+// nie dodałem właściwości board do wszystkich zapytań pobierających todo
--- frontend/todo-app/src/app/start/start.component.ts
+++ frontend/todo-app/src/app/start/start.component.ts
@@ -3,7 +3,10 @@ import { ApiService } from "../api.service";
 import { Apollo } from "apollo-angular";
 import gql from "graphql-tag";
 
-import { createBoard, createTodo, deleteBoard, deleteTodo, board, todo, markTodoAsDone, markTodoAsUndone } from "../graphql/mutations";
+
+// import { createBoard, createTodo, deleteBoard, deleteTodo, board, todo, markTodoAsDone, markTodoAsUndone } from "../graphql/mutations";
+
+import { showBoard, getTodos, getDoneTodos, getUndoneTodos, getTodo } from "../graphql/queries";
 import { variable } from "@angular/compiler/src/output/output_ast";
 
 @Component({
@@ -19,15 +22,16 @@ export class StartComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.apollo.mutate<any>({
-      mutation: markTodoAsUndone,
+    this.apollo.query<any>({
+      query: getTodo,
       variables: {
-        id: "1da8a6a4-e1a3-4c1f-ab65-18c13de4c212",
+        id: "9402c063-399b-4552-a7f2-657613c68f46",
       },
     })
       .subscribe(
         (data: any) => console.log(data)
       );
+
     // this.api.getHelloWorld();
     /*
 
