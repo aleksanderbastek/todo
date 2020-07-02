f8c2d3cd9dc7a3209f6f35d329df27a1c09e9877
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-07-02 11:03:11 +0200

    Improve entrypoint.sh logging of frontend app

--- src/frontend/entrypoint.sh
+++ src/frontend/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Installing node packages (npm install)..."
-npm install
+echo "Starting to install node packages (npm install)"
+echo "It may take few minutes if packages are not already installed"
+echo "Please wait..."
+npm install --loglevel verbose
 
-echo "Starting application!"
+echo "Starting application..."
 npm run start-in-container
