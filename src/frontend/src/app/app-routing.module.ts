4470056f8c46dd1b56a9bc5c68a9e705f4348c23
aleksanderbastek <officialbastek@gmail.com>
2020-07-13 11:45:10 +0200

    Fix the routing problem

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
 		data: { title: "start" },
 	},
 
-	{ path: "", redirectTo: "", pathMatch: "full" },
+	{ path: "", redirectTo: "/", pathMatch: "full" },
 ];
 
 @NgModule({
