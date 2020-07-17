b7d6eaca8f962f5bda3b0cfaf8fcd23839a7926b
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-07-17 13:31:56 +0200

    Remove verbose loglevel setting for entrypoint for frontend packages restore

--- src/frontend/entrypoint.sh
+++ src/frontend/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Starting to install node packages (npm install)"
 echo "It may take few minutes if packages are not already installed"
 echo "Please wait..."
-npm install --loglevel verbose
+npm install
 
 echo "Starting application..."
 npm run start-in-container
